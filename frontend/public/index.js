6dea417f801bdfe1c056e462c2a702eb6faaea34
nguyenhy <nguyenhy311@gmail.com>
2021-05-20 00:31:26 +0700

    add sample frontend and backend code

---
+++ frontend/public/index.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************!*\
+  !*** ./index.js ***!
+  \******************/
+window.onload = function () {
+  console.log(123);
+};
+/******/ })()
+;
---
+++ frontend/webpack.mix.js
@@ -0,0 +1,3 @@
+let mix = require('laravel-mix');
+
+mix.js('index.js', 'public/index.js');
